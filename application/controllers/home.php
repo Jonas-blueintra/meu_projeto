cba13759b21a36f9a3ce3085ca979930fc2f40bd
jonas anjos <jonas.anjos@blueintra.com.br>
2025-11-13 14:24:58 -0300

    Primeiro commit limpo

---
+++ application/controllers/home.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class home extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/userguide3/general/urls.html
+	 */
+	   public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Bank_consultation');
+    }
+	public function index()
+	{$this->load->model('Bank_consultation');
+    
+    $consulta = $this->Bank_consultation->get_single('usuarios','id',1);
+
+    $data['consulta'] = $consulta;
+    $this->load->view('home', $data);
+
+	}
+}
